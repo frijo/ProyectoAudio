07a55225235f7df1e6936f948c1f45a7376b7c70
Walter <walsalas_91@hotmail.com>
2014-10-19 16:12:12 -0600

    Modificando controlador para el guardado y validacion de audio

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+class HomeController extends BaseController {
+
+
+	public function index()
+	{
+		$this->layout->nest('content', 'index');
+	}
+
+	public function store()
+	{
+		$audio = new Audio();
+		$file = Input::file('filename');
+		$name = $file->getClientOriginalName();
+		$extension=$file->getClientOriginalExtension();
+		$duracion = Input::get('duracion');
+		$opcion = Input::get('opcion');
+		if ($extension=="mp3"||$extension=="wav"||$extension=="ogg") {
+		
+			if ($opcion == "partes") {
+				$audio->parts = $duracion;
+				$audio->time_per_chunk = 0;
+			}
+			else{
+				$audio->parts = 0;
+				$audio->time_per_chunk = $duracion;
+			}
+			
+			$audio->files = $name;
+			$audio->save();
+		}
+		else{
+			Session::flash('message','Incompatible file, please select an audio file with MP3, WAV or OGG extension');
+			Session::flash('class','danger');
+		}
+
+		return Redirect::to('/');
+
+	}
+
+}
